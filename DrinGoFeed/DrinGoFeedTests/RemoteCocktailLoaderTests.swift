3c0c41cb03e524eb846c8b69a4defa0fd7d2464f
Fernando Vega <fvegagiga@gmail.com>
2021-05-02 18:27:00 +0200

    Request data from URL on 'load'

--- DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
+++ DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
@@ -6,18 +6,34 @@ import XCTest
 
 class RemoteCocktailLoader {
     
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteCocktailLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        let sut = RemoteCocktailLoader()
+        let client = HTTPClient.shared
+        _ = RemoteCocktailLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteCocktailLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
