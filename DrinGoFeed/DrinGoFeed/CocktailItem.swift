984ba5bfa6eca6a537461d4abf50825ed33ef1e5
Fernando Vega <fvegagiga@gmail.com>
2021-05-02 13:34:04 +0200

    Create the CocktailItem model

---
+++ DrinGoFeed/DrinGoFeed/CocktailItem.swift
@@ -0,0 +1,14 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+
+struct CocktailItem {
+    let id: Int
+    let name: String
+    let description: String
+    let imageURL: URL
+    let ingredients: [String]
+    let quantity: [String]
+}
