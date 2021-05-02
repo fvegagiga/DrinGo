6a716f82809635f27afaf3b7e75a36dcc0e4f8fa
Fernando Vega <fvegagiga@gmail.com>
2021-05-02 18:38:37 +0200

    Move test logic to a separate type 'HTTPClientSpy'

--- DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
+++ DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
@@ -7,29 +7,37 @@ import XCTest
 class RemoteCocktailLoader {
     
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
     
-    private init() {}
-    
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteCocktailLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteCocktailLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteCocktailLoader()
         
         sut.load()
