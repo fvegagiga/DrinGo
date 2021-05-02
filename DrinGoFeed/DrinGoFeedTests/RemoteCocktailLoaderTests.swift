136b572b82d2e6308dcc53fa675c50e9fe286bd1
Fernando Vega <fvegagiga@gmail.com>
2021-05-02 19:23:21 +0200

    Guarantee that we can load more than once

--- DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
+++ DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
@@ -22,6 +22,16 @@ class RemoteCocktailLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteCocktailLoader, client: HTTPClientSpy) {
@@ -33,9 +43,11 @@ class RemoteCocktailLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
