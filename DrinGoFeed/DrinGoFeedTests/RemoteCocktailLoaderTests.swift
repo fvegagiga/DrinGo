1ce063ef9ddc085051b6013eac0f943ac08cce87
Fernando Vega <fvegagiga@gmail.com>
2021-05-03 09:24:00 +0200

    Delivers invalidData error on 200 HTTP response with invalid json

--- DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
+++ DrinGoFeed/DrinGoFeedTests/RemoteCocktailLoaderTests.swift
@@ -58,6 +58,18 @@ class RemoteCocktailLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedError = [RemoteCocktailLoader.Error]()
+        sut.load { capturedError.append($0) }
+        
+        let invalidJSON = Data("invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteCocktailLoader, client: HTTPClientSpy) {
@@ -82,12 +94,12 @@ class RemoteCocktailLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
