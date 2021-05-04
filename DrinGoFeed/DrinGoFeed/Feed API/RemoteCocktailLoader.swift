42e80d136cbe647ddd1cc0194264e019a1e8ebe1
Fernando Vega <fvegagiga@gmail.com>
2021-05-04 09:30:30 +0200

    Move HTTPClient to its own file

--- DrinGoFeed/DrinGoFeed/Feed API/RemoteCocktailLoader.swift
+++ DrinGoFeed/DrinGoFeed/Feed API/RemoteCocktailLoader.swift
@@ -4,12 +4,6 @@
 
 import Foundation
 
-public protocol HTTPClient {
-    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
-    
-    func get(from url: URL, completion: @escaping (Result) -> Void)
-}
-
 public final class RemoteCocktailLoader {
     private let url: URL
     private let client: HTTPClient
