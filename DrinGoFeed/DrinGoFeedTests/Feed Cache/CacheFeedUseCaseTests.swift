ad8b631a40050b2190cffe6de15f6c855098a65c
Fernando Vega <fvegagiga@gmail.com>
2021-05-12 16:48:57 +0200

    Save command requests cache deletion

--- DrinGoFeed/DrinGoFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ DrinGoFeed/DrinGoFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -3,16 +3,26 @@
 //
 
 import XCTest
+import DrinGoFeed
 
 class LocalFeedLoader {
-    init(store: FeedStore) {
+    var store: FeedStore
     
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [CocktailItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
     
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -24,4 +34,23 @@ class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
 
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(idx: 0), uniqueItem(idx: 1)]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    func uniqueItem(idx: Int) -> CocktailItem {
+        return CocktailItem(id: idx, name: "any", description: "any", imageURL: anyURL(), ingredients: ["Ing1", "Ingr2"], quantity: ["Qt1", "Qt2"])
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
 }
