b2e0716b001c1461c2e7c63b816bb24cd48d72fd
Fernando Vega <fvegagiga@gmail.com>
2021-05-13 08:48:38 +0200

    Move 'LocalFeedLoader' (and 'FeedStore' collaborator) to its own file in production

---
+++ DrinGoFeed/DrinGoFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,43 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [CocktailItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [CocktailItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            
+            completion(error)
+        }
+    }
+}
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [CocktailItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
