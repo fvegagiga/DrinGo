4ee6211112cf444d1ca02d6a19bb39fbc083eb88
Fernando Vega <fvegagiga@gmail.com>
2021-05-14 18:53:26 +0200

    Extract duplicate test code into a shared helper method

--- DrinGoFeed/DrinGoFeed/Feed Cache/LocalFeedLoader.swift
+++ DrinGoFeed/DrinGoFeed/Feed Cache/LocalFeedLoader.swift
@@ -9,6 +9,7 @@ public final class LocalFeedLoader {
     private let currentDate: () -> Date
     
     public typealias SaveResult = Error?
+    public typealias LoadResult = CocktailLoader.Result
     
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -27,7 +28,7 @@ public final class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (CocktailLoader.Result) -> Void) {
+    public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { error in
             if let error = error {
                 completion(.failure(error))
