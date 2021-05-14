0422bf013e86406c9539a09db0c9733644e9ae09
Fernando Vega <fvegagiga@gmail.com>
2021-05-14 08:30:53 +0200

    Add 'RemoteCocktailItem' data transfer representation to decouple the items mapper from 'Cocktail Item' data models.

--- DrinGoFeed/DrinGoFeed/Feed API/CocktailItemMapper.swift
+++ DrinGoFeed/DrinGoFeed/Feed API/CocktailItemMapper.swift
@@ -4,58 +4,36 @@
 
 import Foundation
 
+struct RemoteCocktailItem: Decodable {
+    let idDrink: String
+    let strDrink: String
+    let strInstructions: String
+    let strDrinkThumb: String
+    let strIngredient1: String?
+    let strIngredient2: String?
+    let strIngredient3: String?
+    let strIngredient4: String?
+    let strIngredient5: String?
+    let strMeasure1: String?
+    let strMeasure2: String?
+    let strMeasure3: String?
+    let strMeasure4: String?
+    let strMeasure5: String?
+}
+
 final class CocktailItemMapper {
     private struct Root: Decodable {
-        let drinks: [Drink]
-        
-        var feed: [CocktailItem] {
-            return drinks.compactMap { $0.drink }
-        }
-    }
-
-    private struct Drink: Decodable {
-        let idDrink: String
-        let strDrink: String
-        let strInstructions: String
-        let strDrinkThumb: String
-        let strIngredient1: String?
-        let strIngredient2: String?
-        let strIngredient3: String?
-        let strIngredient4: String?
-        let strIngredient5: String?
-        let strMeasure1: String?
-        let strMeasure2: String?
-        let strMeasure3: String?
-        let strMeasure4: String?
-        let strMeasure5: String?
-        
-        var drink: CocktailItem? {
-            guard let id = Int(idDrink) else { return nil }
-            return CocktailItem(id: id,
-                                name: strDrink,
-                                description: strInstructions,
-                                imageURL: URL(string: strDrinkThumb)!,
-                                ingredients: [strIngredient1,
-                                              strIngredient2,
-                                              strIngredient3,
-                                              strIngredient4,
-                                              strIngredient5].compactMap({$0}),
-                                quantity: [strMeasure1,
-                                           strMeasure2,
-                                           strMeasure3,
-                                           strMeasure4,
-                                           strMeasure5].compactMap({$0}))
-        }
+        let drinks: [RemoteCocktailItem]
     }
     
     private static var OK_200: Int { return 200 }
     
-    static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteCocktailLoader.Result {
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [RemoteCocktailItem] {
         guard response.statusCode == OK_200,
               let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteCocktailLoader.Error.invalidData)
+            throw RemoteCocktailLoader.Error.invalidData
         }
         
-        return .success(root.feed)
+        return root.drinks
     }
 }
