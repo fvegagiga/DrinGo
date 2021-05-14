ffa0333d9c712c80925e6d1448d07fca24568f1e
Fernando Vega <fvegagiga@gmail.com>
2021-05-14 19:24:17 +0200

    Load command delivers no cocktails on more than seven days old cache

--- DrinGoFeed/DrinGoFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ DrinGoFeed/DrinGoFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -61,6 +61,18 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    
+    func test_load_deliversCacheCocktailsOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueCocktails()
+        let fixedCurrentDate = Date()
+        let moreThansevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timestamp: moreThansevenDaysOldTimestamp)
+        })
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
