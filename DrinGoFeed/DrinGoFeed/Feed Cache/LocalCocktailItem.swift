b077777d4458ff839d1a903a9737e613c9bd0688
Fernando Vega <fvegagiga@gmail.com>
2021-05-14 08:36:53 +0200

    Move 'LocalCocktailItem' to its own file

---
+++ DrinGoFeed/DrinGoFeed/Feed Cache/LocalCocktailItem.swift
@@ -0,0 +1,23 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+
+public struct LocalCocktailItem: Equatable {
+    public let id: Int
+    public let name: String
+    public let description: String
+    public let imageURL: URL
+    public let ingredients: [String]
+    public let quantity: [String]
+    
+    public init(id: Int, name: String, description: String, imageURL: URL, ingredients: [String], quantity: [String]) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.imageURL = imageURL
+        self.ingredients = ingredients
+        self.quantity = quantity
+    }
+}
