f96c0757528a376ef0e82f5216c5136a238efe2b
Fernando Vega <fvegagiga@gmail.com>
2021-05-15 20:00:11 +0200

    Insert delivers error on insertion error (invalid store URL)

--- DrinGoFeed/DrinGoFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ DrinGoFeed/DrinGoFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -58,12 +58,16 @@ class CodableFeedStore {
     }
     
     func insert(_ cocktails: [LocalCocktailItem], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: cocktails.map(CodableCocktailItem.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
-        
-        completion(nil)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: cocktails.map(CodableCocktailItem.init), timestamp: timestamp)
+            let encoded = try! encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+            
+        } catch {
+            completion(error)
+        }
     }
 }
 
@@ -145,6 +149,18 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .found(feed: latestFeed, timestamp: latesTimestamp))
     }
     
+    func test_insert_deliversErrorOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")!
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueCocktails().local
+        let timestamp = Date()
+        
+        let insertionError = insert((feed, timestamp), to: sut)
+        
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+        expect(sut, toRetrieve: .empty)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
@@ -160,7 +176,6 @@ class CodableFeedStoreTests: XCTestCase {
         var insertionError: Error?
         sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
             insertionError = receivedInsertionError
-            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
