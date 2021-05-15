a405ec9757c77c97b474f721c2581cfbc8587ddf
Fernando Vega <fvegagiga@gmail.com>
2021-05-15 11:29:22 +0200

    Move 'CocktailCachePolicy' to its own file

---
+++ DrinGoFeed/DrinGoFeed/Feed Cache/CocktailCachePolicy.swift
@@ -0,0 +1,22 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+
+final class CocktailCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
