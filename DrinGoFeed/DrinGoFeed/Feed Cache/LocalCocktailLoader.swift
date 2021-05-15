d7804297710a54bd7bd88484185aa2ecaab3554f
Fernando Vega <fvegagiga@gmail.com>
2021-05-15 11:25:48 +0200

    Make 'CocktailCachePolicy' static since it doesn't keep any state

--- DrinGoFeed/DrinGoFeed/Feed Cache/LocalCocktailLoader.swift
+++ DrinGoFeed/DrinGoFeed/Feed Cache/LocalCocktailLoader.swift
@@ -5,13 +5,15 @@
 import Foundation
 
 private final class CocktailCachePolicy {
-    private let calendar = Calendar(identifier: .gregorian)
+    private init() {}
     
-    private var maxCacheAgeInDays: Int {
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
         return 7
     }
     
-    func validate(_ timestamp: Date, against date: Date) -> Bool {
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
@@ -22,7 +24,6 @@ private final class CocktailCachePolicy {
 public final class LocalCocktailLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
-    private let cachePolicy = CocktailCachePolicy()
     
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -65,7 +66,7 @@ extension LocalCocktailLoader: CocktailLoader {
             case let .failure(error):
                 completion(.failure(error))
 
-            case let .found(items, timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(items, timestamp) where CocktailCachePolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(items.toModels()))
                 
             case .found, .empty:
@@ -84,7 +85,7 @@ extension LocalCocktailLoader {
             case .failure:
                 self.store.deleteCachedFeed { _ in }
                 
-            case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(_, timestamp) where !CocktailCachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCachedFeed { _ in }
                 
             case .empty, .found:
