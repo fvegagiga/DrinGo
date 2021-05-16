d4598bb5308869843c313b570e2ba83e8b373019
Fernando Vega <fvegagiga@gmail.com>
2021-05-16 17:58:27 +0200

    Extract duplicate cache load expectations into a shared helper method

--- DrinGoFeed/DrinGoFeedCacheIntegrationTests/DrinGoFeedCacheIntegrationTests.swift
+++ DrinGoFeed/DrinGoFeedCacheIntegrationTests/DrinGoFeedCacheIntegrationTests.swift
@@ -22,19 +22,7 @@ class DrinGoFeedCacheIntegrationTests: XCTestCase {
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
 
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(cocktails):
-                XCTAssertEqual(cocktails, [], "Expected empty feed")
-                
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
-            }
-            
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toLoad: [])
     }
     
     func test_load_deliversItemsSavedOnASeparateInstance() {
@@ -49,19 +37,7 @@ class DrinGoFeedCacheIntegrationTests: XCTestCase {
         }
         wait(for: [saveExp], timeout: 1.0)
         
-        let loadExp = expectation(description: "Wait for load completion")
-        sutToPerformLoad.load { loadResult in
-            switch loadResult {
-            case let .success(cocktails):
-                XCTAssertEqual(cocktails, feed)
-                
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
-            }
-            
-            loadExp.fulfill()
-        }
-        wait(for: [loadExp], timeout: 1.0)
+        expect(sutToPerformLoad, toLoad: feed)
     }
     
     // MARK: Helpers
@@ -73,6 +49,22 @@ class DrinGoFeedCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func expect(_ sut: LocalCocktailLoader, toLoad expectedItems: [CocktailItem], file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(loadedItems):
+                XCTAssertEqual(loadedItems, expectedItems, file: file, line: line)
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
     }
 
     private func testSpecificStoreURL() -> URL {
