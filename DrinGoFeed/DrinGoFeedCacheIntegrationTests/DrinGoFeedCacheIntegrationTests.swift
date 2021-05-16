06c1aad84c57ecb76124e85a71541ac29bbd174e
Fernando Vega <fvegagiga@gmail.com>
2021-05-16 17:44:26 +0200

    `LocalCocktailLoader` in integration with the `CodableFeedStore` delivers no items on empty cache

--- DrinGoFeed/DrinGoFeedCacheIntegrationTests/DrinGoFeedCacheIntegrationTests.swift
+++ DrinGoFeed/DrinGoFeedCacheIntegrationTests/DrinGoFeedCacheIntegrationTests.swift
@@ -3,9 +3,44 @@
 //
 
 import XCTest
+import DrinGoFeed
 
 class DrinGoFeedCacheIntegrationTests: XCTestCase {
 
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
 
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(cocktails):
+                XCTAssertEqual(cocktails, [], "Expected empty feed")
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalCocktailLoader {
+        let storeURL = testSpecificStoreURL()
+        let store = CodableFeedStore(storeURL: storeURL)
+        let sut = LocalCocktailLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
