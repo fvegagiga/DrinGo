48e2925e69e682713662665eced125e964fdbfce
Fernando Vega <fvegagiga@gmail.com>
2021-05-16 08:41:03 +0200

    Move 'CodableFeedStore' to its own file production

---
+++ DrinGoFeed/DrinGoFeed/Feed Cache/CodableFeedStore.swift
@@ -0,0 +1,84 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+
+public class CodableFeedStore: FeedStore {
+    private struct Cache: Codable {
+        let feed: [CodableCocktailItem]
+        let timestamp: Date
+        
+        var localCocktails: [LocalCocktailItem] {
+            return feed.map { $0.local }
+        }
+    }
+    
+    private struct CodableCocktailItem: Codable {
+        private let id: Int
+        private let name: String
+        private let description: String
+        private let imageURL: URL
+        private let ingredients: [String]
+        private let quantity: [String]
+        
+        init(_ cocktail: LocalCocktailItem) {
+            self.id = cocktail.id
+            self.name = cocktail.name
+            self.description = cocktail.description
+            self.imageURL = cocktail.imageURL
+            self.ingredients = cocktail.ingredients
+            self.quantity = cocktail.quantity
+        }
+        
+        var local: LocalCocktailItem {
+            LocalCocktailItem(id: id, name: name, description: description, imageURL: imageURL, ingredients: ingredients, quantity: quantity)
+        }
+    }
+    
+    private let storeURL: URL
+
+    public init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.localCocktails, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    public func insert(_ cocktails: [LocalCocktailItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: cocktails.map(CodableCocktailItem.init), timestamp: timestamp)
+            let encoded = try! encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+            
+        } catch {
+            completion(error)
+        }
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        guard FileManager.default.fileExists(atPath: storeURL.path) else {
+            return completion(nil)
+        }
+        
+        do {
+            try FileManager.default.removeItem(at: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+    }
+}
