7b9cbfb37a834c6aadead5e3762ae99247e0625e
Fernando Vega <fvegagiga@gmail.com>
2021-05-20 18:26:23 +0200

    Add 'CoctailListViewController' rendering 10 cells

--- Prototype/Prototype/CocktailListViewController.swift
+++ Prototype/Prototype/CocktailListViewController.swift
@@ -1,3 +1,15 @@
-// 
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
 
-import Foundation
+import UIKit
+
+final class CoctailListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "CocktailImageCell")!
+    }
+}
