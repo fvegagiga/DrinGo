b5771566a559ef3263a540001f5f44d84a8fe90e
Fernando Vega <fvegagiga@gmail.com>
2021-05-21 17:22:52 +0200

    Load feed on view did load

--- DrinGoFeed/DrinGoFeediOSTests/CocktailFeedViewControllerTests.swift
+++ DrinGoFeed/DrinGoFeediOSTests/CocktailFeedViewControllerTests.swift
@@ -3,10 +3,20 @@
 //
 
 import XCTest
+import UIKit
 
-final class CocktailFeedViewController {
-    init(loader: CocktailFeedViewControllerTests.LoaderSpy) {
+final class CocktailFeedViewController: UIViewController {
+    private var loader: CocktailFeedViewControllerTests.LoaderSpy?
+
+    convenience init(loader: CocktailFeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader?.load()
     }
 }
 
@@ -18,10 +28,23 @@ class CocktailFeedViewControllerTests: XCTestCase {
         
         XCTAssertEqual(loader.loadCallCount, 0)
     }
+    
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = CocktailFeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
 
     // MARK: - Helpers
     
     class LoaderSpy {
         private (set) var loadCallCount: Int = 0
+    
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
