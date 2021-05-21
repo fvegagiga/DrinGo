f220a3ea3009055ee7f4ce34f99fcf91e8d30bab
Fernando Vega <fvegagiga@gmail.com>
2021-05-21 08:42:34 +0200

    Make 'DrinGoFeed' and 'DrinGoFeedTest' targets support macOS and iOS since they're platform-independent (can run on any platform). As a consequence, is needed to change cache directory access used to pass tests because the permissions are different in both platforms.

--- DrinGoFeed/DrinGoFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ DrinGoFeed/DrinGoFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -118,15 +118,13 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     }
     
     func test_delete_deliversErrorOnDeletionError() {
-        let noDeletePermissionURL = cachesDirectory()
-        let sut = makeSUT(storeURL: noDeletePermissionURL)
+        let sut = makeSUT(storeURL: noDeletePermissionURL())
         
         assertThatDeleteDeliversErrorOnDeletionError(on: sut)
     }
     
     func test_delete_hasNoSideEffectsOnDeletionError() {
-        let noDeletePermissionURL = cachesDirectory()
-        let sut = makeSUT(storeURL: noDeletePermissionURL)
+        let sut = makeSUT(storeURL: noDeletePermissionURL())
         
         assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
     }
@@ -148,6 +146,11 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     
     private func setupEmptyStoreState() {
         deleteStoreArtifacts()
+        createCachesDirectory()
+    }
+    
+    private func createCachesDirectory() {
+        try? FileManager.default.createDirectory(atPath: cachesDirectory().path, withIntermediateDirectories: true, attributes: nil)
     }
     
     private func undoStoreSideEffects() {
@@ -164,5 +167,9 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func noDeletePermissionURL() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .systemDomainMask).first!
     }
 }
