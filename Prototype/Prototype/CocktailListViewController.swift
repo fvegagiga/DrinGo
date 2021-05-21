73487ae483c77473da8d8d45cc19bd055cf96b9b
Fernando Vega <fvegagiga@gmail.com>
2021-05-21 07:22:36 +0200

    Render prototype cocktail data on the table view

--- Prototype/Prototype/CocktailListViewController.swift
+++ Prototype/Prototype/CocktailListViewController.swift
@@ -12,10 +12,23 @@ struct CocktailImageViewModel {
 
 final class CocktailListViewController: UITableViewController {
     
+    private let cocktails = CocktailImageViewModel.prototypeFeed
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return cocktails.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "CocktailImageCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CocktailImageCell", for: indexPath) as! CocktailImageCell
+        let model = cocktails[indexPath.row]
+        cell.configure(with: model)
+        return cell
+    }
+}
+
+extension CocktailImageCell {
+    func configure(with model: CocktailImageViewModel) {
+        titleLabel.text = model.title
+        cocktailImageView.image = UIImage(named: model.imageName)
+        descriptionLabel.text = model.description
     }
 }
