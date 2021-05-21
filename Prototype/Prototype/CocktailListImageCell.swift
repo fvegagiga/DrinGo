56172bb1126ceaf4647e83acebe2f4b7c0492054
Fernando Vega <fvegagiga@gmail.com>
2021-05-21 07:12:51 +0200

    Add 'CocktailImageCell' with storyboard connections

---
+++ Prototype/Prototype/CocktailListImageCell.swift
@@ -0,0 +1,12 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import UIKit
+
+final class CocktailImageCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var cocktailImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
