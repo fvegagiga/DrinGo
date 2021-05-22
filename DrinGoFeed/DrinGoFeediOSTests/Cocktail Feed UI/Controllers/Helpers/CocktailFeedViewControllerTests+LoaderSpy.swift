d3c474e98b6c3754ec5def51fcf00a5bddbe9ca2
Fernando Vega <fvegagiga@gmail.com>
2021-05-22 08:22:43 +0200

    Extract LoaderSpy to a separate file

---
+++ DrinGoFeed/DrinGoFeediOSTests/Cocktail Feed UI/Controllers/Helpers/CocktailFeedViewControllerTests+LoaderSpy.swift
@@ -0,0 +1,62 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+import DrinGoFeed
+import DrinGoFeediOS
+
+class LoaderSpy: CocktailLoader, CocktailImageDataLoader {
+    
+    // MARK: - FeedLoader
+    
+    private struct TaskSpy: CocktailImageDataLoaderTask {
+        let cancelCallback: () -> Void
+        func cancel() {
+            cancelCallback()
+        }
+    }
+
+    private var feedRequests = [(CocktailLoader.Result) -> Void]()
+    
+    var loadFeedCallCount: Int {
+        return feedRequests.count
+    }
+    
+    func load(completion: @escaping (CocktailLoader.Result) -> Void) {
+        feedRequests.append(completion)
+    }
+    
+    func completeFeedLoading(with feed: [CocktailItem] = [], at index: Int = 0) {
+        feedRequests[index](.success(feed))
+    }
+    
+    func completeFeedLoadingWithError(at index: Int = 0) {
+        let error = NSError(domain: "an error", code: 0)
+        feedRequests[index](.failure(error))
+    }
+    
+    // MARK: - FeedImageDataLoader
+
+    private var imageRequests = [(url: URL, completion: (CocktailImageDataLoader.Result) -> Void)]()
+
+    var loadedImageURLs: [URL] {
+        return imageRequests.map { $0.url }
+    }
+
+    private(set) var cancelledImageURLs = [URL]()
+
+    func loadImageData(from url: URL, completion: @escaping (CocktailImageDataLoader.Result) -> Void) -> CocktailImageDataLoaderTask {
+        imageRequests.append((url, completion))
+        return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url) }
+    }
+    
+    func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
+        imageRequests[index].completion(.success(imageData))
+    }
+    
+    func completeImageLoadingWithError(at index: Int = 0) {
+        let error = NSError(domain: "an error", code: 0)
+        imageRequests[index].completion(.failure(error))
+    }
+}
