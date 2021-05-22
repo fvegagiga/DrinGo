49121a217ffbb8ae8ea03bcf4401e1ae64775db6
Fernando Vega <fvegagiga@gmail.com>
2021-05-22 08:10:38 +0200

    Extract UIButton test helper to a separate file

---
+++ DrinGoFeed/DrinGoFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,15 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
