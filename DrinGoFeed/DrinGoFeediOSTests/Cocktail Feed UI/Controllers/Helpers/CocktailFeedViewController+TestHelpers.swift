894b8cd7ecd30e78336149f613df8d24d7992060
Fernando Vega <fvegagiga@gmail.com>
2021-05-22 08:19:14 +0200

    Extract CocktailFeedViewController test helpers to a separate file

---
+++ DrinGoFeed/DrinGoFeediOSTests/Cocktail Feed UI/Controllers/Helpers/CocktailFeedViewController+TestHelpers.swift
@@ -0,0 +1,57 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import UIKit
+import DrinGoFeediOS
+
+extension CocktailFeedViewController {
+    @discardableResult
+    func simulateFeedImageViewVisible(at index: Int) -> CocktailFeedCell? {
+        return coktailFeedView(at: index) as? CocktailFeedCell
+    }
+    
+    func simulateFeedImageViewNotVisible(at row: Int) {
+        let view = simulateFeedImageViewVisible(at: row)
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+    }
+    
+    func simulateFeedImageViewNearVisible(at row: Int) {
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView(tableView, prefetchRowsAt: [index])
+    }
+
+    func simulateFeedImageViewNotNearVisible(at row: Int) {
+        simulateFeedImageViewNearVisible(at: row)
+        
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
+
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+    
+    func numberOfRenderedFeedImageViews() -> Int {
+        return tableView.numberOfRows(inSection: feedImagesSection)
+    }
+    
+    func coktailFeedView(at row: Int) -> UITableViewCell? {
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
+
+    private var feedImagesSection: Int {
+        return 0
+    }
+}
