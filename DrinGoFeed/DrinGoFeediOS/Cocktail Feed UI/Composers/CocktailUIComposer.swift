31379d7c4afa4be5b2b194bace03c0c37a333b2e
Fernando Vega <fvegagiga@gmail.com>
2021-05-23 08:23:50 +0200

    Move `MainQueueDispatchDecorator` to separate file

--- DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/CocktailUIComposer.swift
+++ DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/CocktailUIComposer.swift
@@ -23,40 +23,6 @@ public final class CocktailUIComposer {
     }
 }
 
-private final class MainQueueDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        
-        completion()
-    }
-}
-
-extension MainQueueDispatchDecorator: CocktailLoader where T == CocktailLoader {
-
-    func load(completion: @escaping (CocktailLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
-extension MainQueueDispatchDecorator: CocktailImageDataLoader where T == CocktailImageDataLoader {
-    
-    func loadImageData(from url: URL, completion: @escaping (CocktailImageDataLoader.Result) -> Void) -> CocktailImageDataLoaderTask {
-        decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
 private extension CocktailFeedViewController {
     static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> CocktailFeedViewController {
         let bundle = Bundle(for: CocktailFeedViewController.self)
---
+++ DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/MainQueueDispatchDecorator.swift
@@ -0,0 +1,40 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+import DrinGoFeed
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: CocktailLoader where T == CocktailLoader {
+
+    func load(completion: @escaping (CocktailLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
+
+extension MainQueueDispatchDecorator: CocktailImageDataLoader where T == CocktailImageDataLoader {
+    
+    func loadImageData(from url: URL, completion: @escaping (CocktailImageDataLoader.Result) -> Void) -> CocktailImageDataLoaderTask {
+        decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
