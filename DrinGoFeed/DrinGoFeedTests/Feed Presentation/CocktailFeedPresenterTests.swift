28b582cbb80391bc5af7968c5af01c257728974c
Fernando Vega <fvegagiga@gmail.com>
2021-05-23 12:20:50 +0200

    Extract system under test creation to a test factory method

--- DrinGoFeed/DrinGoFeedTests/Feed Presentation/CocktailFeedPresenterTests.swift
+++ DrinGoFeed/DrinGoFeedTests/Feed Presentation/CocktailFeedPresenterTests.swift
@@ -13,17 +13,23 @@ final class CocktailFeedPresenter {
 class CocktailFeedPresenterTests: XCTestCase {
 
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
-
-        _ = CocktailFeedPresenter(view: view)
+        let (_, view) = makeSUT()
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
     // MARK: - Helpers
 
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: CocktailFeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = CocktailFeedPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+
+    
     private class ViewSpy {
         let messages = [Any]()
     }
-    
 }
