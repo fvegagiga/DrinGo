185eabb26bfc2ea175a1c408afb5edbd36d13989
Fernando Vega <fvegagiga@gmail.com>
2021-05-23 08:29:57 +0200

    Move `FeedViewAdapter` to separate file

--- DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/CocktailUIComposer.swift
+++ DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/CocktailUIComposer.swift
@@ -33,26 +33,3 @@ private extension CocktailFeedViewController {
         return cocktailFeedController
     }
 }
-
-private final class FeedViewAdapter: FeedView {
-    private weak var controller: CocktailFeedViewController?
-    private let imageLoader: CocktailImageDataLoader
-    
-    init(controller: CocktailFeedViewController, imageLoader: CocktailImageDataLoader) {
-        self.controller = controller
-        self.imageLoader = imageLoader
-    }
-    
-    func display(_ viewModel: FeedViewModel) {
-        controller?.tableModel = viewModel.feed.map { model in
-            let adapter = CocktailImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<CocktailFeedCellController>, UIImage>(model: model, imageLoader: imageLoader)
-            let view = CocktailFeedCellController(delegate: adapter)
-            
-            adapter.presenter = CocktailImagePresenter(
-                view: WeakRefVirtualProxy(view),
-                imageTransformer: UIImage.init)
-            
-            return view
-        }
-    }
-}
---
+++ DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,28 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import UIKit
+
+final class FeedViewAdapter: FeedView {
+    private weak var controller: CocktailFeedViewController?
+    private let imageLoader: CocktailImageDataLoader
+    
+    init(controller: CocktailFeedViewController, imageLoader: CocktailImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map { model in
+            let adapter = CocktailImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<CocktailFeedCellController>, UIImage>(model: model, imageLoader: imageLoader)
+            let view = CocktailFeedCellController(delegate: adapter)
+            
+            adapter.presenter = CocktailImagePresenter(
+                view: WeakRefVirtualProxy(view),
+                imageTransformer: UIImage.init)
+            
+            return view
+        }
+    }
+}
