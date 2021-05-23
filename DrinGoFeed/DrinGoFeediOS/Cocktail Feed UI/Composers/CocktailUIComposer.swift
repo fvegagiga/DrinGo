2ebf763baea49642709f199ea701ce596cc7d5db
Fernando Vega <fvegagiga@gmail.com>
2021-05-23 08:26:41 +0200

    Move `CocktailImageDataLoaderPresentationAdapter` to separate file

--- DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/CocktailUIComposer.swift
+++ DrinGoFeed/DrinGoFeediOS/Cocktail Feed UI/Composers/CocktailUIComposer.swift
@@ -79,35 +79,3 @@ private final class CocktailFeedLoaderPresentationAdapter: FeedViewControllerDel
         }
     }
 }
-
-private final class CocktailImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-    private let model: CocktailItem
-    private let imageLoader: CocktailImageDataLoader
-    private var task: CocktailImageDataLoaderTask?
-    
-    var presenter: CocktailImagePresenter<View, Image>?
-    
-    init(model: CocktailItem, imageLoader: CocktailImageDataLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-    
-    func didRequestImage() {
-        presenter?.didStartLoadingImageData(for: model)
-        
-        let model = self.model
-        task = imageLoader.loadImageData(from: model.imageURL) { [weak self] result in
-            switch result {
-            case let .success(data):
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-                
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-            }
-        }
-    }
-    
-    func didCancelImageRequest() {
-        task?.cancel()
-    }
-}
