ec5b1c122db8935fa415c0f7c632666338d2d595
Fernando Vega <fvegagiga@gmail.com>
2021-05-23 12:45:58 +0200

    Move prod types to the DrinGo production module

---
+++ DrinGoFeed/DrinGoFeed/Feed Presentation/CocktailFeedPresenter.swift
@@ -0,0 +1,79 @@
+//
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [CocktailItem]
+}
+
+public protocol FeedView {
+    func display(_ viewModel: FeedViewModel)
+}
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
+
+public protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
+
+public protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+public final class CocktailFeedPresenter {
+    private let feedView: FeedView
+    private let loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
+    
+    public static var title: String {
+        return NSLocalizedString("COCKTAIL_LIST_VIEW_TITLE",
+            tableName: "CocktailFeed",
+            bundle: Bundle(for: CocktailFeedPresenter.self),
+            comment: "Title for the feed view")
+    }
+    
+    public var feedLoadError: String {
+        return NSLocalizedString("COCKTAIL_LIST_VIEW_CONNECTION_ERROR",
+            tableName: "CocktailFeed",
+            bundle: Bundle(for: CocktailFeedPresenter.self),
+            comment: "Error message displayed when we can't load the image feed from the server")
+    }
+
+    
+    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+    
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+        
+    public func didFinishLoadingFeed(with feed: [CocktailItem]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+
+    public func didFinishLoadingFeed(with error: Error) {
+        errorView.display(.error(message: feedLoadError))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+}
