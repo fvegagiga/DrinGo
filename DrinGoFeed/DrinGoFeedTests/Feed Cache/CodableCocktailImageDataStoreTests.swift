1c70aab15541e54b7cfe20b9908a01b55f0738d9
Fernando Vega <fvegagiga@gmail.com>
2021-05-29 18:21:58 +0200

    `CodableFeedStore.retrieveImageData` delivers `image data not found` when store is not empty but there's no image with matching URL

--- DrinGoFeed/DrinGoFeedTests/Feed Cache/CodableCocktailImageDataStoreTests.swift
+++ DrinGoFeed/DrinGoFeedTests/Feed Cache/CodableCocktailImageDataStoreTests.swift
@@ -23,6 +23,17 @@ class CodableCocktailImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: notFound(), for: anyURL())
     }
     
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() {
+        let sut = makeSUT()
+        let url = URL(string: "http://a-url.com")!
+        let nonMatchingURL = URL(string: "http://another-url.com")!
+        
+        insert(anyData(), for: url, into: sut)
+        
+        expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
+    }
+
+    
     // - MARK: Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
@@ -52,6 +63,32 @@ class CodableCocktailImageDataStoreTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    private func insert(_ data: Data, for url: URL, into sut: CodableFeedStore, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for cache insertion")
+        let image = localImage(url: url)
+        sut.insert([image], timestamp: Date()) { result in
+            switch result {
+            case let .failure(error):
+                XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+                
+            case .success:
+                sut.insert(data, for: url) { result in
+                    if case let Result.failure(error) = result {
+                        XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
+                    }
+                }
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    
+    private func localImage(url: URL) -> LocalCocktailItem {
+        return LocalCocktailItem(id: 0, name: "name", description: "description", imageURL: url, ingredients: ["ing1"], quantity: ["qt1"])
+    }
+
 
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
