5fa93b093adeea4e1c9287705fb04d0d36a2a812
Fernando Vega <fvegagiga@gmail.com>
2021-05-30 11:36:29 +0200

    `CocktailImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first

--- DrinGoApp/DrinGoAppTests/CocktailImageDataLoaderWithFallbackCompositeTests.swift
+++ DrinGoApp/DrinGoAppTests/CocktailImageDataLoaderWithFallbackCompositeTests.swift
@@ -6,17 +6,21 @@ import XCTest
 import DrinGoFeed
 
 class CocktailImageDataLoaderWithFallbackComposite: CocktailImageDataLoader {
+    private let primary: CocktailImageDataLoader
+    
+    init(primary: CocktailImageDataLoader, fallback: CocktailImageDataLoader) {
+        self.primary = primary
+    }
+
     private class Task: CocktailImageDataLoaderTask {
         func cancel() {
             
         }
     }
     
-    init(primary: CocktailImageDataLoader, fallback: CocktailImageDataLoader) {
-        
-    }
-    
     func loadImageData(from url: URL, completion: @escaping (CocktailImageDataLoader.Result) -> Void) -> CocktailImageDataLoaderTask {
+        _ = primary.loadImageData(from: url) { _ in }
+
         return Task()
     }
 }
@@ -32,7 +36,23 @@ class CocktailImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
         
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = CocktailImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+
     // MARK: - Helpers
+    
+    func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
     
     private class LoaderSpy: CocktailImageDataLoader {
         private var messages = [(url: URL, completion: (CocktailImageDataLoader.Result) -> Void)]()
