85286124a796473b4c9f6a28c9be1cd83dc5682a
Fernando Vega <fvegagiga@gmail.com>
2021-05-30 11:56:20 +0200

    Extract memory leak tracking helper into a shared scope to remove duplication

--- DrinGoApp/DrinGoAppTests/CocktailImageDataLoaderWithFallbackCompositeTests.swift
+++ DrinGoApp/DrinGoAppTests/CocktailImageDataLoaderWithFallbackCompositeTests.swift
@@ -124,13 +124,6 @@ class CocktailImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
-    
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-
     func anyNSError() -> NSError {
         NSError(domain: "any error", code: 0)
     }
--- DrinGoApp/DrinGoAppTests/CocktailLoaderWithFallbackCompositeTests.swift
+++ DrinGoApp/DrinGoAppTests/CocktailLoaderWithFallbackCompositeTests.swift
@@ -62,13 +62,6 @@ class CocktailLoaderWithFallbackCompositeTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
-
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-
     func uniqueCocktail(id: Int = 0) -> [CocktailItem] {
         return [CocktailItem(id: id, name: "any", description: "any", imageURL: anyURL(), ingredients: ["Ing1", "Ingr2"], quantity: ["Qt1", "Qt2"])]
     }
