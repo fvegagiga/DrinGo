09cf4e0b320907320dec01667e0b52a468e3f17c
Fernando Vega <fvegagiga@gmail.com>
2021-05-30 08:55:20 +0200

    Add empty DrinGoApp project

---
+++ DrinGoApp/DrinGoApp/ViewController.swift
@@ -0,0 +1,14 @@
+// Copyright @ 2021 Fernando Vega. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
