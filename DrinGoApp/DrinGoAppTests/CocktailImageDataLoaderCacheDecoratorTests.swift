0e28a73a8acc8a4c4a14e7fbe9e0086279565b3e
Fernando Vega <fvegagiga@gmail.com>
2021-05-31 14:22:58 +0200

    `CocktailImageDataLoaderCacheDecorator.loadImageData` caches loaded data on loader success

--- DrinGoApp/DrinGoAppTests/CocktailImageDataLoaderCacheDecoratorTests.swift
+++ DrinGoApp/DrinGoAppTests/CocktailImageDataLoaderCacheDecoratorTests.swift
@@ -6,18 +6,28 @@ import XCTest
 import DrinGoFeed
 import DrinGoApp
 
+protocol CocktailImageDataCache {
+    typealias Result = Swift.Result<Void, Error>
+
+    func save(_ data: Data, for url: URL, completion: @escaping (Result) -> Void)
+}
+
 class CocktailImageDataLoaderCacheDecorator: CocktailImageDataLoader {
     private let decoratee: CocktailImageDataLoader
-    
-    init(decoratee: CocktailImageDataLoader) {
+    private let cache: CocktailImageDataCache
+
+    init(decoratee: CocktailImageDataLoader, cache: CocktailImageDataCache) {
         self.decoratee = decoratee
+        self.cache = cache
     }
     
     func loadImageData(from url: URL, completion: @escaping (CocktailImageDataLoader.Result) -> Void) -> CocktailImageDataLoaderTask {
-        return decoratee.loadImageData(from: url, completion: completion)
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            self?.cache.save((try? result.get()) ?? Data(), for: url) { _ in }
+            completion(result)
+        }
     }
 }
-
 
 class CocktailImageDataLoaderCacheDecoratorTests: XCTestCase, CocktailImageDataLoaderTestsCase {
 
@@ -63,13 +73,40 @@ class CocktailImageDataLoaderCacheDecoratorTests: XCTestCase, CocktailImageDataL
         })
     }
 
+    func test_loadImageData_cachesLoadedDataOnLoaderSuccess() {
+        let cache = CacheSpy()
+        let url = anyURL()
+        let imageData = anyData()
+        let (sut, loader) = makeSUT(cache: cache)
+
+        _ = sut.loadImageData(from: url) { _ in }
+        loader.complete(with: imageData)
+
+        XCTAssertEqual(cache.messages, [.save(data: imageData, for: url)], "Expected to cache loaded image data on success")
+    }
+
+    
     // MARK: - Helpers
         
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: CocktailImageDataLoader, loader: CocktailImageDataLoaderSpy) {
+    private func makeSUT(cache: CacheSpy = .init(), file: StaticString = #file, line: UInt = #line) -> (sut: CocktailImageDataLoader, loader: CocktailImageDataLoaderSpy) {
         let loader = CocktailImageDataLoaderSpy()
-        let sut = CocktailImageDataLoaderCacheDecorator(decoratee: loader)
+        let sut = CocktailImageDataLoaderCacheDecorator(decoratee: loader, cache: cache)
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
     }
+    
+    private class CacheSpy: CocktailImageDataCache {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case save(data: Data, for: URL)
+        }
+        
+        func save(_ data: Data, for url: URL, completion: @escaping (CocktailImageDataCache.Result) -> Void) {
+            messages.append(.save(data: data, for: url))
+            completion(.success(()))
+        }
+    }
+
 }
