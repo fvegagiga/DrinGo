55476db75da3d34a49777846918ed1223a81432c
Fernando Vega <fvegagiga@gmail.com>
2021-06-04 11:20:20 +0200

    Move Combine helpers to a new file

--- DrinGoApp/DrinGoApp/SceneDelegate.swift
+++ DrinGoApp/DrinGoApp/SceneDelegate.swift
@@ -39,10 +39,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func configureWindow() {
-        
-        let remoteCocktailImageLoader = RemoteCocktailImageDataLoader(client: httpClient)
-        let localImageLoader = LocalCocktailImageDataLoader(store: store)
-        
+
         let cocktailViewController = CocktailUIComposer.feedComposedWith(
             feedLoader: makeRemoteCocktailLoaderWithLocalFallback,
             imageLoader: makeLocalImageDataLoaderWithRemoteFallback)
@@ -78,122 +75,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                         .loadImageDataPublisher(from: url)
                         .caching(to: localImageLoader, using: url)
             })
-    }
-}
-
-// MARK: - CocktailImageDataLoader
-
-public extension CocktailImageDataLoader {
-    typealias Publisher = AnyPublisher<Data, Error>
-    
-    func loadImageDataPublisher(from url: URL) -> Publisher {
-        var task: CocktailImageDataLoaderTask?
-        
-        return Deferred {
-            Future { completion in
-                task = self.loadImageData(from: url, completion: completion)
-            }
-        }
-        .handleEvents(receiveCancel: { task?.cancel() })
-        .eraseToAnyPublisher()
-    }
-}
-
-extension Publisher where Output == Data {
-    func caching(to cache: CocktailImageDataCache, using url: URL) -> AnyPublisher<Output, Failure> {
-        handleEvents(receiveOutput: { data in
-            cache.saveIgnoringResult(data, for: url)
-        }).eraseToAnyPublisher()
-    }
-}
-
-private extension CocktailImageDataCache {
-    func saveIgnoringResult(_ data: Data, for url: URL) {
-        save(data, for: url) { _ in }
-    }
-}
-
-// MARK: - CocktailLoader
-
-public extension CocktailLoader {
-    typealias Publisher = AnyPublisher<[CocktailItem], Error>
-    
-    func loadPublisher() -> Publisher {
-        return Deferred {
-            Future(self.load)
-        }
-        .eraseToAnyPublisher()
-    }
-}
-
-extension Publisher where Output == [CocktailItem] {
-    func caching(to cache: CocktailCache) -> AnyPublisher<Output, Failure> {
-        handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
-    }
-}
-
-private extension CocktailCache {
-    func saveIgnoringResult(_ feed: [CocktailItem]) {
-        save(feed) { _ in }
-    }
-}
-
-extension Publisher {
-    func fallback(to fallbackPublisher: @escaping () -> AnyPublisher<Output, Failure>) -> AnyPublisher<Output, Failure> {
-        self.catch { _ in fallbackPublisher() }.eraseToAnyPublisher()
-    }
-}
-
-extension Publisher {
-    func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
-        receive(on: DispatchQueue.inmediateWhenOnMainQueueScheduler).eraseToAnyPublisher()
-    }
-}
-
-extension DispatchQueue {
-    static var inmediateWhenOnMainQueueScheduler: InmediateWhenOnMainQueueScheduler {
-        InmediateWhenOnMainQueueScheduler.shared
-    }
-    
-    struct InmediateWhenOnMainQueueScheduler: Scheduler {
-        typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
-        typealias SchedulerOptions = DispatchQueue.SchedulerOptions
-        
-        var now: Self.SchedulerTimeType {
-            DispatchQueue.main.now
-        }
-
-        var minimumTolerance: Self.SchedulerTimeType.Stride {
-            DispatchQueue.main.minimumTolerance
-        }
-
-        static let shared = Self()
-        
-        private static let key = DispatchSpecificKey<UInt8>()
-        private static let value = UInt8.max
-        
-        private init() {
-            DispatchQueue.main.setSpecific(key: Self.key, value: Self.value)
-        }
-        
-        private func isMainQueue() -> Bool {
-            DispatchQueue.getSpecific(key: Self.key) == Self.value
-        }
-        
-        func schedule(options: Self.SchedulerOptions?, _ action: @escaping () -> Void) {
-            guard isMainQueue() else {
-                return DispatchQueue.main.schedule(options: options, action)
-            }
-            
-            action()
-        }
-
-        func schedule(after date: Self.SchedulerTimeType, tolerance: Self.SchedulerTimeType.Stride, options: Self.SchedulerOptions?, _ action: @escaping () -> Void) {
-            DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
-        }
-
-        func schedule(after date: Self.SchedulerTimeType, interval: Self.SchedulerTimeType.Stride, tolerance: Self.SchedulerTimeType.Stride, options: Self.SchedulerOptions?, _ action: @escaping () -> Void) -> Cancellable {
-            DispatchQueue.main.schedule(after: date, interval: interval, tolerance: tolerance, options: options, action)
-        }
     }
 }
