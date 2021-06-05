c16909e4de2accafe6361dd0c952cd1aedfa2238
Fernando Vega <fvegagiga@gmail.com>
2021-06-05 14:47:56 +0200

    Remove unused helper

--- DrinGoFeed/DrinGoFeedTests/Feed API/CocktailItemMapperTests.swift
+++ DrinGoFeed/DrinGoFeedTests/Feed API/CocktailItemMapperTests.swift
@@ -116,27 +116,4 @@ class CocktailItemMapperTests: XCTestCase {
         
         return (item, json)
     }
-    
-    private func expect(_ sut: RemoteCocktailLoader, toCompleteWith expectedResult: RemoteCocktailLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedItems), .success(expectedItems)):
-                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-            
-            case let (.failure(receivedError as RemoteCocktailLoader.Error), .failure(expectedError as RemoteCocktailLoader.Error)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-                
-            default:
-                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
-    }
 }
