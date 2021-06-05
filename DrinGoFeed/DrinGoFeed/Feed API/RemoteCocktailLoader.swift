0f598adae426060bf3766012864f98baf4e993d3
Fernando Vega <fvegagiga@gmail.com>
2021-06-05 13:48:55 +0200

    Replace RemoteCocktailLoader with generic RemoteLoader to remove duplication

--- DrinGoFeed/DrinGoFeed/Feed API/RemoteCocktailLoader.swift
+++ DrinGoFeed/DrinGoFeed/Feed API/RemoteCocktailLoader.swift
@@ -4,41 +4,10 @@
 
 import Foundation
 
-public final class RemoteCocktailLoader: CocktailLoader {
-    private let url: URL
-    private let client: HTTPClient
-    
-    public typealias Result = CocktailLoader.Result
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteCocktailLoader.map(data, from: response))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let cocktails = try CocktailItemMapper.map(data, response)
-            return .success(cocktails)
-        } catch {
-            return .failure(error)
-        }
+public typealias RemoteCocktailLoader = RemoteLoader<[CocktailItem]>
+
+public extension RemoteCocktailLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: CocktailItemMapper.map)
     }
 }
