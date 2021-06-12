6cfc849b223186a0ee6e806b174f61f3f2cf12d2
Fernando Vega <fvegagiga@gmail.com>
2021-06-12 09:09:42 +0200

    Display error on mapper error

--- DrinGoFeed/DrinGoFeed/Shared Presentation/LoadResourcePresenter.swift
+++ DrinGoFeed/DrinGoFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -9,7 +9,7 @@ public protocol ResourceView {
 }
 
 public final class LoadResoucePresenter<Resource, View: ResourceView> {
-    public typealias Mapper = (Resource) -> View.ResourceViewModel
+    public typealias Mapper = (Resource) throws -> View.ResourceViewModel
     
     private let resourceView: View
     private let loadingView: ResourceLoadingView
@@ -36,8 +36,12 @@ public final class LoadResoucePresenter<Resource, View: ResourceView> {
     }
         
     public func didFinishLoading(with resource: Resource) {
-        resourceView.display(mapper(resource))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        do {
+            resourceView.display(try mapper(resource))
+            loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        } catch {
+            didFinishLoading(with: error)
+        }
     }
 
     public func didFinishLoading(with error: Error) {
