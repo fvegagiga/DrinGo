ba07659763a911d40b2a03ff3857cbef207b1678
Fernando Vega <fvegagiga@gmail.com>
2021-06-14 13:07:15 +0200

    Configure `ErrorView` programmatically so we don't duplicate layout logic in storyboards

--- DrinGoFeed/DrinGoFeediOS/Shared UI/Views/ErrorView.swift
+++ DrinGoFeed/DrinGoFeediOS/Shared UI/Views/ErrorView.swift
@@ -4,19 +4,37 @@
 
 import UIKit
 
-public final class ErrorView: UIView {
+public final class ErrorView: UIButton {
     
-    @IBOutlet private var label: UILabel!
-
     public var message: String? {
-        get { return isVisible ? label.text : nil }
+        get { return isVisible ? title(for:.normal) : nil }
         set { setMessageAnimated(newValue) }
     }
     
-    public override func awakeFromNib() {
-        super.awakeFromNib()
+    public var onHide: (() -> Void)?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configure() {
+        backgroundColor = .errorBackgroundColor
         
-        alpha = 0
+        addTarget(self, action: #selector(hideMessageAnimated), for: .touchUpInside)
+        configureLabel()
+        hideMessage()
+    }
+    
+    private func configureLabel() {
+        titleLabel?.textColor = .white
+        titleLabel?.textAlignment = .center
+        titleLabel?.numberOfLines = 0
+        titleLabel?.font = .systemFont(ofSize: 17)
     }
     
     private var isVisible: Bool {
@@ -32,19 +50,32 @@ public final class ErrorView: UIView {
     }
 
     private func showAnimated(_ message: String) {
-        label.text = message
+        setTitle(message, for: .normal)
+        contentEdgeInsets = .init(top: 8, left: 8, bottom: 8, right: 8)
         
         UIView.animate(withDuration: 0.25) {
             self.alpha = 1
         }
     }
     
-    @IBAction private func hideMessageAnimated() {
+    @objc private func hideMessageAnimated() {
         UIView.animate(
             withDuration: 0.25,
             animations: { self.alpha = 0 },
             completion: { completed in
-                if completed { self.label.text = nil }
+                if completed { self.hideMessage() }
             })
+    }
+    
+    private func hideMessage() {
+        setTitle(nil, for: .normal)
+        alpha = 0
+        contentEdgeInsets = .init(top: -2.5, left: 0, bottom: -2.5, right: 0)
+    }
+}
+
+extension UIColor {
+    static var errorBackgroundColor: UIColor {
+        UIColor(red: 0.99951404330000004, green: 0.41759261489999999, blue: 0.4154433012, alpha: 1)
     }
 }
